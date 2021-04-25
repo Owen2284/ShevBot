595cadfbb0bd9ff02a8f3ecf7f226d8b5ed0db21
Owen Shevlin <owen2284@gmail.com>
2021-04-26 00:23:06 +0100

    Commented out broken old commands

--- commands/Social/topic.js
+++ commands/Social/topic.js
@@ -8,32 +8,32 @@ module.exports = {
 	channelTypes: ["text"],
 	permissions: [],
 	process: function(args, client, message) {
-		const Casual = require("casual");
-		const Moniker = require("moniker");
+		// const Casual = require("casual");
+		// const Moniker = require("moniker");
 		
-		const message = manifest.message.message;
-		var topicString = "";
-		var randVal = Math.random() * 4;
-		if (randVal < 1) {
-			topicString = Casual.catch_phrase;
-		} else if (randVal < 2) {
-			topicString = Casual.full_name;
-		} else if (randVal < 3) {
-			var user = Tools.arrays.getRandomElement(Tools.discord.getAllGuildMembers(channel));;
-			var gen = Moniker.generator([Moniker.adjective, Moniker.noun]);
-			if (user.nickname != null) {
-				topicString = user.nickname;
-			} else {
-				topicString = user.user.username;
-			}
-			topicString += "'s " + gen.choose().replace(/-/g, " ");
-		} else {
-			var gen = Moniker.generator([Moniker.adjective, Moniker.adjective, Moniker.noun]);
-			topicString = gen.choose().replace(/-/g, " ");
-			topicString = topicString.substring(0,1).toUpperCase() + topicString.substring(1);
-		}
-		var endings = [" discussion.", ", agree or disagree?", " observations.", " fan club.", ", and how it effects you.", " debating.", " circlejerk.", " 101."]
-		var messageString = topicString + endings[Math.floor(Math.random() * endings.length)];
-		channel.setTopic(messageString);
+		// const message = manifest.message.message;
+		// var topicString = "";
+		// var randVal = Math.random() * 4;
+		// if (randVal < 1) {
+		// 	topicString = Casual.catch_phrase;
+		// } else if (randVal < 2) {
+		// 	topicString = Casual.full_name;
+		// } else if (randVal < 3) {
+		// 	var user = Tools.arrays.getRandomElement(Tools.discord.getAllGuildMembers(channel));;
+		// 	var gen = Moniker.generator([Moniker.adjective, Moniker.noun]);
+		// 	if (user.nickname != null) {
+		// 		topicString = user.nickname;
+		// 	} else {
+		// 		topicString = user.user.username;
+		// 	}
+		// 	topicString += "'s " + gen.choose().replace(/-/g, " ");
+		// } else {
+		// 	var gen = Moniker.generator([Moniker.adjective, Moniker.adjective, Moniker.noun]);
+		// 	topicString = gen.choose().replace(/-/g, " ");
+		// 	topicString = topicString.substring(0,1).toUpperCase() + topicString.substring(1);
+		// }
+		// var endings = [" discussion.", ", agree or disagree?", " observations.", " fan club.", ", and how it effects you.", " debating.", " circlejerk.", " 101."]
+		// var messageString = topicString + endings[Math.floor(Math.random() * endings.length)];
+		// channel.setTopic(messageString);
 	}
 }
--- commands/Voice/voice.js
+++ commands/Voice/voice.js
@@ -18,72 +18,72 @@ module.exports = {
 	channelTypes: ["text"],
 	permissions: [],
 	process: function(args, client, message) {
-		const message = manifest.message.message;
-		const input = manifest.command.full;
-		switch(input[1].toUpperCase()) {
-			case "JOIN":
-				if (bot.voice.connections.array().length == 0) {
-					if (input.length > 2) {
-						var targetChannelRaw = manifest.message.raw.substring(1 + input[0].length + 1 + input[1].length + 1);
-						var targetChannelUpp = targetChannelRaw.toUpperCase();
-						var channelsChannels = manifest.channel.guild.channels.cache.array();
-						var joined = false;
-						var joinedName = "general";
-						for (chch = 0; chch < channelsChannels.length; chch++) {
-							checkChannel = channelsChannels[chch];
-							var theNameOfThis = checkChannel.name.toUpperCase();
-							if (theNameOfThis === targetChannelUpp && checkChannel.constructor.name === "VoiceChannel") {
-								joinedName = checkChannel.name;
-								checkChannel.join();
-								joined = true;
-								break;
-							}
-						}
-						if (joined) {
-							cmd("voice", "Joined voice channel \"" + joinedName + "\".");
-							say("send", message, "Okay, I'll speak on the voice channel \"" + joinedName + "\"!");
-						} else {
-							say("send", message, "Sorry, I couldn't find a voice channel called \"" + targetChannelRaw + "\" on this server...");
-						}
-					} else {
-						say("send", message, "Please enter a voice channel on this server after \"" + details.commandCharacter + "VOICE JOIN\".");
-					} 
-				} else if (bot.voice.connections.array().length == 1) {
-					say("send", message, "Sorry, I'm already speaking on \"" + bot.voice.connections.array()[0].channel.name + "\".");
-				} else {
-					cmd("voice", "ShevBot is currently on > 1 voice channel. This is bad.");
-				} break;
-			case "SWITCH":
-				say("send", message, "Sorry, I haven't had that command programmed in yet!");
-			case "LEAVE":
-				if (bot.voice.connections.array().length == 1) {
-					if (settings.currentStreamDispatcher != null) {settings.currentStreamDispatcher.end(); settings.currentStreamDispatcher == null;}
-					var currentChannel = bot.voice.connections.array()[0].channel;
-					currentChannel.leave();
-					cmd("voice", "Left voice channel \"" + currentChannel.name + "\".");
-					say("send", message, "I have stopped taking on the voice channel \"" + currentChannel.name + "\".");
-				} else if(bot.voice.connections.array().length == 0) {
-					say("send", message, "Sorry, I'm not speaking on a voice channel at the moment.");
-				} else {
-					cmd("whoops", "ShevBot is currently on > 1 voice channel. This is bad.");
-				} break;
-			case "SPEAK":
-				if (bot.voice.connections.array().length == 1) {
-					var currentConnection = bot.voice.connections.array()[0];
-					var soundFileNames = fs.readdirSync(details.soundDir);
-					var soundToPlay = soundFileNames[Math.floor((Math.random() * soundFileNames.length))];
-					var soundToPlayAbsolutePath = path.join(__dirname, details.soundDir + soundToPlay);
-					if (settings.currentStreamDispatcher != null) {settings.currentStreamDispatcher.end(); settings.currentStreamDispatcher == null;}
-					settings.currentStreamDispatcher = currentConnection.play(soundToPlayAbsolutePath, {volume: "0.25" });
-					cmd("voice", "Playing \"" + soundToPlay + "\"");
-				} else if(bot.voice.connections.array().length == 0) {
-					say("send", message, "Sorry, I'm not speaking on a voice channel at the moment.");
-				} else {
-					cmd("whoops", "ShevBot is attempting to speak on more than one channel. Stopping speak request.");
-				} break;
-			default:
-				say("send", message, "The " + details.commandCharacter + "voice command needs another argument after it, such as \"join\"  or \"leave\".");
-				break;
-		}
+		// const message = manifest.message.message;
+		// const input = manifest.command.full;
+		// switch(input[1].toUpperCase()) {
+		// 	case "JOIN":
+		// 		if (bot.voice.connections.array().length == 0) {
+		// 			if (input.length > 2) {
+		// 				var targetChannelRaw = manifest.message.raw.substring(1 + input[0].length + 1 + input[1].length + 1);
+		// 				var targetChannelUpp = targetChannelRaw.toUpperCase();
+		// 				var channelsChannels = manifest.channel.guild.channels.cache.array();
+		// 				var joined = false;
+		// 				var joinedName = "general";
+		// 				for (chch = 0; chch < channelsChannels.length; chch++) {
+		// 					checkChannel = channelsChannels[chch];
+		// 					var theNameOfThis = checkChannel.name.toUpperCase();
+		// 					if (theNameOfThis === targetChannelUpp && checkChannel.constructor.name === "VoiceChannel") {
+		// 						joinedName = checkChannel.name;
+		// 						checkChannel.join();
+		// 						joined = true;
+		// 						break;
+		// 					}
+		// 				}
+		// 				if (joined) {
+		// 					cmd("voice", "Joined voice channel \"" + joinedName + "\".");
+		// 					say("send", message, "Okay, I'll speak on the voice channel \"" + joinedName + "\"!");
+		// 				} else {
+		// 					say("send", message, "Sorry, I couldn't find a voice channel called \"" + targetChannelRaw + "\" on this server...");
+		// 				}
+		// 			} else {
+		// 				say("send", message, "Please enter a voice channel on this server after \"" + details.commandCharacter + "VOICE JOIN\".");
+		// 			} 
+		// 		} else if (bot.voice.connections.array().length == 1) {
+		// 			say("send", message, "Sorry, I'm already speaking on \"" + bot.voice.connections.array()[0].channel.name + "\".");
+		// 		} else {
+		// 			cmd("voice", "ShevBot is currently on > 1 voice channel. This is bad.");
+		// 		} break;
+		// 	case "SWITCH":
+		// 		say("send", message, "Sorry, I haven't had that command programmed in yet!");
+		// 	case "LEAVE":
+		// 		if (bot.voice.connections.array().length == 1) {
+		// 			if (settings.currentStreamDispatcher != null) {settings.currentStreamDispatcher.end(); settings.currentStreamDispatcher == null;}
+		// 			var currentChannel = bot.voice.connections.array()[0].channel;
+		// 			currentChannel.leave();
+		// 			cmd("voice", "Left voice channel \"" + currentChannel.name + "\".");
+		// 			say("send", message, "I have stopped taking on the voice channel \"" + currentChannel.name + "\".");
+		// 		} else if(bot.voice.connections.array().length == 0) {
+		// 			say("send", message, "Sorry, I'm not speaking on a voice channel at the moment.");
+		// 		} else {
+		// 			cmd("whoops", "ShevBot is currently on > 1 voice channel. This is bad.");
+		// 		} break;
+		// 	case "SPEAK":
+		// 		if (bot.voice.connections.array().length == 1) {
+		// 			var currentConnection = bot.voice.connections.array()[0];
+		// 			var soundFileNames = fs.readdirSync(details.soundDir);
+		// 			var soundToPlay = soundFileNames[Math.floor((Math.random() * soundFileNames.length))];
+		// 			var soundToPlayAbsolutePath = path.join(__dirname, details.soundDir + soundToPlay);
+		// 			if (settings.currentStreamDispatcher != null) {settings.currentStreamDispatcher.end(); settings.currentStreamDispatcher == null;}
+		// 			settings.currentStreamDispatcher = currentConnection.play(soundToPlayAbsolutePath, {volume: "0.25" });
+		// 			cmd("voice", "Playing \"" + soundToPlay + "\"");
+		// 		} else if(bot.voice.connections.array().length == 0) {
+		// 			say("send", message, "Sorry, I'm not speaking on a voice channel at the moment.");
+		// 		} else {
+		// 			cmd("whoops", "ShevBot is attempting to speak on more than one channel. Stopping speak request.");
+		// 		} break;
+		// 	default:
+		// 		say("send", message, "The " + details.commandCharacter + "voice command needs another argument after it, such as \"join\"  or \"leave\".");
+		// 		break;
+		// }
 	}
 }

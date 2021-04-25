8e73939967f11d0357a2cfdbcc873a062fd6b02c
Owen Shevlin <owen2284@gmail.com>
2021-04-26 00:19:28 +0100

    Reformatted some code

--- commands/General/info.js
+++ commands/General/info.js
@@ -10,8 +10,10 @@ module.exports = {
 	process: function(args, client, message) {
 		var info = "Hi, I'm ShevBot!\n";
 		info += "I'm a bot capable of running helpful commands, parsing and responding to messages, and more!\n\n";
+
 		info += "Created by Owen Shevlin, running since 6th August 2016.\n"
 		info += "GitHub Repo available at: " + client.config.bot.githubRepo + ".\n\n";
+		
 		info += "To see a full list of the commands I understand, simply type \"" + client.config.bot.commandCharacter + "HELP\".\n";
 		info += "Looking forward to getting to know you!";
 
